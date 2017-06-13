35b5b45b0c5f9f33ac663418714807c16fb95812
jon <jbrenluc@gmail.com>
2017-06-13 01:34:21 -0500

    cleaned up stuff. sub.check_call or sub.Popen?

--- vassemble.py
+++ vassemble.py
@@ -1,40 +1,38 @@
 import subprocess as sub
 import sys
 import os
+
 class VAssemble:
-    def __init__(self, finput, qc_flag, r_type):
-        self.finput = args.finput
-        self.quality_control = qc_flag 
-        self.r_type = 'SE'
+    def __init__(self, finput, asm_dir, paired_reads):
+        self.finput = finput
+        self.asm_dir = asm_dir 
+        self.paired_reads = paired_reads
 
     def run_qc(self, qc_dir):
-        self.qc_dir = qc_dir 
-        p = self._run_sickle()
-        print(p.stdout)
-        print(p.stderr, file=sys.stderr)
+        self.qc_dir = qc_dir
+        self._run_sickle()
+
         # TODO redirect input to trimmed files
         # TODO implement logging
 
     def _run_sickle(self):
         s_type = 'sanger'
-        s_length = '100' 
-        if self.r_type == 'SE':
+        s_length = '100'
+        if not self.paired_reads:
             fname = os.path.basename(self.finput[0])
-            p = sub.run(['sh',
+            p = sub.check_call([
                          'sickle',
                           self.r_rtype,
                           '-f', self.finput[0],
                           '-t', s_type,
                           '-l', s_length,
                           '-o', os.path.join(self.qc_dir, 'trimmed-'+fname)
-
-                        ],
-                        stdout=sub.PIPE, stderr=sub.PIPE, check=True
+                        ]
                        )
         else:
             fname = os.path.basename(self.finput[0])
             rname = os.path.basename(self.finput[1])
-            p = sub.run([
+            p = sub.check_call([
                          'sickle',
                          self.r_type,
                          '-f', self.finput[0],
@@ -44,7 +42,5 @@ class VAssemble:
                          '-o', os.path.join(self.qc_dir, 'trimemed-'+fname),
                          '-p', os.path.join(self.qc_dir, 'trimmed-'+rname),
                          '-s', os.path.join(self.qc_dir, 'singletons.fastq')
-                        ],
-                        stdout=sub.PIPE, stderr=sub.PIPE, check=True
-                       )
-            return p
+                         ])
+        return p
--- virusland.py
+++ virusland.py
@@ -1,11 +1,18 @@
 import sys
-
 import vutils
 import vassemble
+
 # pass list of arguments to VSetup
 vconf = vutils.VSetup(sys.argv[1:])
 vconf.check_dependencies()
 
-vasm = vassemble.VAssemble(vconf.args, vconf.out_dirs)
-if vconf.args.quality_control == True:
-    vasm.run_qc()
+vasm = vassemble.VAssemble(
+                            vconf.args.finput,
+                            vconf.out_dirs['assembled'],
+                            vconf.args.paried_end_reads
+                          )
+
+if vconf.args.quality_control is True:
+    vasm.run_qc( vconf.out_dirs['trimmed'])
+
+if not vconf.args.assembled_contigs:
