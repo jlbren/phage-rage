b9c40d7aa944a002d091c96c53ca922ba31ecbe5
jlbren <jbrenluc@gmail.com>
2017-06-14 23:32:32 -0500

    formated input in vassemble

--- vassemble.py
+++ vassemble.py
@@ -1,6 +1,7 @@
 import subprocess as sub
 import sys
 import os
+from glob import glob
 
 class VAssemble:
     def __init__(self, finput, asm_dir, paired_reads):
@@ -11,27 +12,53 @@ class VAssemble:
     def run_qc(self, qc_dir):
         self.qc_dir = qc_dir
         self._run_sickle()
-
-        # TODO redirect input to trimmed files
+        self.finput = glob(self.qc_dir)
+        # TODO redirect input to trimmed files/
         # TODO implement logging
+    def run_assembly(self, assembler):
+        self.assembler = assembler
+        asm_input = _to_formatted_input()
+        _run_assembler(asm_input)
+
+    def _to_formatted_input(self):
+        if self.paired_end_reads is True:
+            trimmed = [f for f in finput if 'trimmed' in f] # glorious one liner
+        if self.assembler == 'velvet':
+            # Velvet just takes all files as positional args. 
+            return self.finput
+
+        elif self.assembler == 'spades':
+            if self.paired_reads is True:
+                pass
+            else:
+                pass
+                
+        elif self.assembler == 'megahit':
+            # Megahit does not do SE or singletons. 
+            return ['-1', trimmed[0], '-2', trimmed[1]]
 
     def _run_sickle(self):
         s_type = 'sanger'
         s_length = '100'
+        foutput = []
         if not self.paired_reads:
-            fname = os.path.basename(self.finput[0])
+            fname = 'trimmed-'+os.path.basename(self.finput[0])
+            fouput.append(os.path.join(self.qc_dir, fname))
             p = sub.check_call([
                          'sickle',
                           'se',
                           '-f', self.finput[0],
                           '-t', s_type,
                           '-l', s_length,
-                          '-o', os.path.join(self.qc_dir, 'trimmed-'+fname)
+                          '-o', foutput[0]
                         ]
                        )
         else:
-            fname = os.path.basename(self.finput[0])
-            rname = os.path.basename(self.finput[1])
+            fname = 'trimmed-' + os.path.basename(self.finput[0])
+            rname = 'trimmed-' + os.path.basename(self.finput[1])
+            foutput.append(os.path.join(self.qc_dir, fname))
+            foutput.append(os.path.join(self.qc_dir, rname))
+
             p = sub.check_call([
                          'sickle',
                          'pe',
