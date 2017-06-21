1d8b451a8b2bed808f92667b4e1f9de4d6aaa661
jon <jbrenluc@gmail.com>
2017-06-21 01:34:41 -0500

    mapper almost there

--- vmap.py
+++ vmap.py
@@ -8,9 +8,10 @@ class VMap:
         self.finput = finput
         self.threads = threads 
 
-    def run_map(self, mapper, map_dir):
+    def run_map(self, mapper, index_dir, map_dir):
         self.mapper = mapper
         self.map_dir = map_dir
+        self.index_dir = index_dir 
     
     def get_orfs(self):
         pass 
@@ -20,13 +21,28 @@ class VMap:
             pass
 
         elif self.mapper == 'pauda':
-            pass
+            print('Running pauda...')
+            subprocess.check_call(['pauda-run',
+                                   self.finpunt,
+                                   self.mapper_dir,
+                                   self.index_dir
+
+                                  ])
+            
 
         elif self.mapper == 'lambda':
-            pass 
+            print('Running lambda...')
+            subprocess.check_call(['lambda',
+                                   '-q', self.finput,
+                                   '-i', self.index_dir
+                                  ])
 
         elif self.mapper == 'diamond':
-            pass
+            subprocess.check_call(['diamond', 'blastp',
+                                   '-d', self.index_dir,
+                                   '-q', self.finput,
+                                   '-o', self.mapper_dir
+                                  ])
 
         else:
             pass # TODO Same old problem with these cases
