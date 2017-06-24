e4631ccf364b7a6e216ff1c8d5fcd30e8b59aa72
bio tech 2 <biotech2@bio>
2017-06-23 23:07:46 -0500

    cleaned up unecessary files

--- getorf.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def runGetOrf(input_contigs, output, find_parameter):
-    #translations of regions between STOP codons: default setting of 0
-    #translations of regions between STOP codons: nucleotide setting is 2
-    os.system('getorf -find '+ find_parameter + ' ' + input_contigs + ' ' + output)
-
-runGetOrf('contigs.fa', 'test0Output', '0')
-runGetOrf('contigs.fa', 'test2Output', '2')
