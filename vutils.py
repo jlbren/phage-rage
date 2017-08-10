cf08b8c15c779b214715b23f41e3b1064daf93c7
thatzopoulos <thatzopoulos965@gmail.com>
2017-08-09 20:51:57 -0500

    minor fix

--- vutils.py
+++ vutils.py
@@ -170,7 +170,7 @@ class VSetup:
             if name == 'spades':
                 return ['spades.py']
             elif name == 'velvet':
-                return['velvelth', 'velvetg']
+                return['velveth', 'velvetg']
             elif name == 'megahit':
                 return ['megahit']
         elif type == 'mapper':
